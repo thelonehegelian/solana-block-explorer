5d8d059dc73973a39ca8d30104e64582db262f99
thelonehegelian <thelonehegelian@gmail.com>
2023-02-10 13:11:34 +0000

    Add getBlock function

--- main.go
+++ main.go
@@ -11,6 +11,53 @@ import (
 	"github.com/joho/godotenv"
 )
 
+type Block struct {
+	Jsonrpc string `json:"jsonrpc"`
+
+	Result struct {
+		BlockHeight       int         `json:"blockHeight"`
+		BlockTime         interface{} `json:"blockTime"`
+		Blockhash         string      `json:"blockhash"`
+		ParentSlot        int         `json:"parentSlot"`
+		PreviousBlockhash string      `json:"previousBlockhash"`
+		Transactions      []struct {
+			Meta struct {
+				Err               interface{}   `json:"err"`
+				Fee               int           `json:"fee"`
+				InnerInstructions []interface{} `json:"innerInstructions"`
+				LogMessages       []interface{} `json:"logMessages"`
+				PostBalances      []interface{} `json:"postBalances"`
+				PostTokenBalances []interface{} `json:"postTokenBalances"`
+				PreBalances       []interface{} `json:"preBalances"`
+				PreTokenBalances  []interface{} `json:"preTokenBalances"`
+				Rewards           interface{}   `json:"rewards"`
+				Status            struct {
+					Ok interface{} `json:"Ok"`
+				} `json:"status"`
+			} `json:"meta"`
+			Transaction struct {
+				Message struct {
+					AccountKeys []string `json:"accountKeys"`
+					Header      struct {
+						NumReadonlySignedAccounts   int `json:"numReadonlySignedAccounts"`
+						NumReadonlyUnsignedAccounts int `json:"numReadonlyUnsignedAccounts"`
+						NumRequiredSignatures       int `json:"numRequiredSignatures"`
+					} `json:"header"`
+					Instructions []struct {
+						Accounts       []int  `json:"accounts"`
+						Data           string `json:"data"`
+						ProgramIDIndex int    `json:"programIdIndex"`
+					} `json:"instructions"`
+					RecentBlockhash string `json:"recentBlockhash"`
+				} `json:"message"`
+				Signatures []string `json:"signatures"`
+			} `json:"transaction"`
+		} `json:"transactions"`
+	} `json:"result"`
+	ID int `json:"id"`
+}
+
+// ////////
 func createRequestMessage(methodName string, params []interface{}) []byte {
 	request := struct {
 		JSONRPC string        `json:"jsonrpc"`
@@ -31,6 +78,31 @@ func createRequestMessage(methodName string, params []interface{}) []byte {
 
 	return requestMessage
 }
+
+func getBlock(blockNumber int, url string) (Block, error) {
+	var blockResponse Block
+
+	methodName := "getBlock"
+	params := []interface{}{blockNumber, map[string]interface{}{
+		"encoding":                       "json",
+		"maxSupportedTransactionVersion": 0,
+		"transactionDetails":             "full",
+		"rewards":                        false,
+	}}
+
+	requestMessage := createRequestMessage(methodName, params)
+	response, err := http.Post(url, "application/json", bytes.NewBuffer(requestMessage))
+	responseBytes, err := ioutil.ReadAll(response.Body)
+
+	// read into blockResponse
+	err = json.Unmarshal(responseBytes, &blockResponse)
+	if err != nil {
+		fmt.Println("Error unmarshalling block response:", err)
+	}
+
+	return blockResponse, err
+}
+
 func main() {
 
 	err := godotenv.Load()
@@ -44,14 +116,13 @@ func main() {
 	// url := baseUrl + apiKey
 	url := "https://api.mainnet-beta.solana.com"
 
-	// Get Epoch Info
+	blockData, err := getBlock(1, url)
 
-	requestMessage := createRequestMessage("getEpochInfo", []interface{}{})
+	// requestMessage := createRequestMessage(methodName, params)
+	// response, err := http.Post(url, "application/json", bytes.NewBuffer(requestMessage))
+	// responseBytes, err := ioutil.ReadAll(response.Body)
+	// fmt.Println("response:", string(responseBytes))
 
-	response, err := http.Post(url, "application/json", bytes.NewBuffer(requestMessage))
-
-	responseBytes, err := ioutil.ReadAll(response.Body)
-
-	fmt.Println("getEpochInfoRequestResponseBytes:", string(responseBytes))
+	fmt.Println("blockData:", blockData.Result.Transactions[0].Meta.Fee)
 
 }
